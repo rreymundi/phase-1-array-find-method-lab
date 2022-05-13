99418da14fa2c3e9a565dadd011de46ed88f22aa
raul reymundi <kinskispiral@gmail.com>
2022-05-13 16:21:34 -0400

    commit tree f1010fc7379ea7a65161c3133af93cef8fcdbe84

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+
+
+// approach #1
+// function superbowlWin(array){
+//     if (array.result === `W`){
+//         return array.year;
+//     }
+//     else {
+//         return undefined;
+//     }
+// }
+// array.find(superbowlWin);
+
+
+// approach #2
+// function superbowlWin(array){
+//     let winningYear = array.find(object => object.result === `W` ? object.year : undefined);
+//     return winningYear;
+// }
+
+// approach #3
+function superbowlWin(array){
+    let winningYear = array.find(object => object.result === `W`);
+    if (winningYear){
+        return winningYear.year
+    }
+}
